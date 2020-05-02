eec056c058fdacfd737ec6bc04b58bb6db9f56ac
Ganesh Durai <ganiths@gmail.com>
2020-05-02 14:03:32 +0100

    Updated Google cloud deployment configuration

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t ganiths/multi-client:latest -t ganiths/multi-client:$GIT_SHA ./client/Dockerfile ./client
+docker build -t ganiths/multi-server:latest -t ganiths/multi-server:$GIT_SHA ./server/Dockerfile ./server
+docker build -t ganiths/multi-worker:latest -t ganiths/multi-worker:$GIT_SHA ./worker/Dockerfile ./worker
+docker push ganiths/multi-client:latest
+docker push ganiths/multi-server:latest
+docker push ganiths/multi-worker:latest
+docker push ganiths/multi-client:$GIT_SHA
+docker push ganiths/multi-server:$GIT_SHA
+docker push ganiths/multi-worker:$GIT_SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ganiths/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=ganiths/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment client=ganiths/multi-worker:$GIT_SHA
